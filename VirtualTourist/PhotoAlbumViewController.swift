351154d0be8bba2a7287277ec0400ce3d625f4a0
cpg6b <chris.garvey@gmail.com>
2016-04-10 19:42:59 -0700

    feat: Add image download functionality

--- VirtualTourist/PhotoAlbumViewController.swift
+++ VirtualTourist/PhotoAlbumViewController.swift
@@ -16,7 +16,10 @@ func performUIUpdatesOnMain(updates: () -> Void) {
     }
 }
 
-class PhotoAlbumViewController: UIViewController {
+/* Set reuse identifier constant for use in this file only */
+private let reuseIdentifier = "PhotoCollectionViewCell"
+
+class PhotoAlbumViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
     // MARK: - Properties
     
@@ -25,7 +28,9 @@ class PhotoAlbumViewController: UIViewController {
     @IBOutlet weak var errorMessage: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
     
-    var photos: [String]!
+    var photos = [String]()
+    
+    
     
     // MARK: - Life Cycle
 
@@ -39,10 +44,9 @@ class PhotoAlbumViewController: UIViewController {
                 // call the NSFetchResults controller to fetch the photos that have been saved for this pin
                 
                 performUIUpdatesOnMain {
-                    self.photos = photos
-                    self.errorMessage.text = "Success"
-                    self.errorMessage.hidden = false
+                    self.photos = photos!
                     print(self.photos)
+                    self.collectionView!.reloadData()
                 }
             
             } else {
@@ -57,18 +61,66 @@ class PhotoAlbumViewController: UIViewController {
     }
 
     
-    // TO DO: Check the old app from networking class that used flickr to find networking code
+    // MARK: - Collection View Layout
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        // Lay out the collection view so that cells take up 1/3 of the width,
+        // with 5 points space in between.
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.minimumLineSpacing = 5
+        layout.minimumInteritemSpacing = 5
+        
+        let width = floor((self.collectionView.frame.size.width-10)/3)
+        layout.itemSize = CGSize(width: width, height: width)
+        collectionView.collectionViewLayout = layout
+    }
+   
     
     
+    // MARK: - Collection View Data Source Protocol Method - Getting Item and Section Metrics
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
     
     
+    // MARK: - Collection View Data Source Protocol Method - Getting Views For Items
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        print("Reached cellForItemAtIndexPath")
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! PhotoCollectionViewCell
+        cell.backgroundColor = UIColor.blackColor()
+        cell.activityIndicator.startAnimating()
+        
+        // citation: http://stackoverflow.com/questions/28868894/swift-url-reponse-is-nil
+        let session = NSURLSession.sharedSession()
+        let imageURL = NSURL(string: photos[indexPath.row])
+        
+        let sessionTask = session.dataTaskWithURL(imageURL!) { data, response, error in
+            performUIUpdatesOnMain {
+                cell.photoImageView?.image = UIImage(data: data!)
+                cell.activityIndicator.stopAnimating()
+            }
+        }
+        
+        sessionTask.resume()
+        return cell
+        
+    }
+    
+    // MARK: - Collection View Delegate Protocol Method - Managing the Selected Cells
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        // TO DO: have the photo selected and then deleted if necessary
+    }
     
     
-    
-    // TO DO: Set up the networking though using separate networking class and MVC
+   
     // TO DO: Add the photos to the Core Data framework
     // TO DO: Use NSFetchController functionality to drive the collection view
-    // TO DO: Add activity indicator for each cell as it's loading
     // TO DO: Create "New Collection" functionality
     
     
---
+++ VirtualTourist/PhotoCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoCollectionViewCell.swift
+//  VirtualTourist
+//
+//  Created by Chris Garvey on 4/10/16.
+//  Copyright © 2016 Chris Garvey. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    /* Property to hold the image for the custom collection view cell */
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+}
+
